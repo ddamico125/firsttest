2fa1d7aa760a25a7c9917f76f1596ea4c13d5eb5
ddamico125 <35408737+ddamico125@users.noreply.github.com>
2018-07-17 13:56:18 -0400

    Create sigma app

---
+++ firsttest/lambda.js
@@ -0,0 +1,17 @@
+let AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
+exports.handler = function (event, context, callback) {
+	ddb.get({
+		TableName: 'test_user_tabel',
+		Key: { 'id': 'id' }
+	}, function (err, data) {
+		if (err) {
+			//handle error
+		} else {
+			//your logic goes here
+		}
+	});
+
+
+	callback(null, 'Successfully executed');
+}
